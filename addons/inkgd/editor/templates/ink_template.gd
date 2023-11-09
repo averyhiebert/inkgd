f8e2245aada3c83a0059b754cf1393313161c402
Avery Hiebert <averyhiebert@gmail.com>
2023-11-09 00:30:05 -0500

    Update ink_template.gd (non-signal version only)

--- addons/inkgd/editor/templates/ink_template.gd
+++ addons/inkgd/editor/templates/ink_template.gd
@@ -1,6 +1,6 @@
 # warning-ignore-all:return_value_discarded
 
-extends %BASE%
+extends _BASE_
 
 # ############################################################################ #
 # Imports
@@ -13,7 +13,7 @@ var InkPlayer = load("res://addons/inkgd/ink_player.gd")
 # ############################################################################ #
 
 # Alternatively, it could also be retrieved from the tree.
-# onready var _ink_player = $InkPlayer
+# @onready var _ink_player = $InkPlayer
 @onready var _ink_player = InkPlayer.new()
 
 # ############################################################################ #
@@ -21,22 +21,22 @@ var InkPlayer = load("res://addons/inkgd/ink_player.gd")
 # ############################################################################ #
 
 func _ready():
-%TS%# Adds the player to the tree.
-%TS%add_child(_ink_player)
+_TS_# Adds the player to the tree.
+_TS_add_child(_ink_player)
 
-%TS%# Replace the example path with the path to your story.
-%TS%# Remove this line if you set 'ink_file' in the inspector.
-%TS%_ink_player.ink_file = load("res://path/to/file.ink.json")
+_TS_# Replace the example path with the path to your story.
+_TS_# Remove this line if you set 'ink_file' in the inspector.
+_TS__ink_player.ink_file = load("res://path/to/file.ink.json")
 
-%TS%# It's recommended to load the story in the background. On platforms that
-%TS%# don't support threads, the value of this variable is ignored.
-%TS%_ink_player.loads_in_background = true
+_TS_# It's recommended to load the story in the background. On platforms that
+_TS_# don't support threads, the value of this variable is ignored.
+_TS__ink_player.loads_in_background = true
 
-%TS%_ink_player.connect("loaded", Callable(self, "_story_loaded"))
+_TS__ink_player.loaded.connect(_story_loaded)
 
-%TS%# Creates the story. 'loaded' will be emitted once Ink is ready
-%TS%# continue the story.
-%TS%_ink_player.create_story()
+_TS_# Creates the story. 'loaded' will be emitted once Ink is ready
+_TS_# continue the story.
+_TS__ink_player.create_story()
 
 
 # ############################################################################ #
@@ -44,13 +44,13 @@ func _ready():
 # ############################################################################ #
 
 func _story_loaded(successfully: bool):
-%TS%if !successfully:
-%TS%%TS%return
+_TS_if !successfully:
+_TS__TS_return
 
-%TS%# _observe_variables()
-%TS%# _bind_externals()
+_TS_# _observe_variables()
+_TS_# _bind_externals()
 
-%TS%_continue_story()
+_TS__continue_story()
 
 
 # ############################################################################ #
@@ -58,44 +58,44 @@ func _story_loaded(successfully: bool):
 # ############################################################################ #
 
 func _continue_story():
-%TS%while _ink_player.can_continue:
-%TS%%TS%var text = _ink_player.continue_story()
-%TS%%TS%# This text is a line of text from the ink story.
-%TS%%TS%# Set the text of a Label to this value to display it in your game.
-%TS%%TS%print(text)
-%TS%if _ink_player.has_choices:
-%TS%%TS%# 'current_choices' contains a list of the choices, as strings.
-%TS%%TS%for choice in _ink_player.current_choices:
-%TS%%TS%%TS%print(choice.text)
-%TS%%TS%%TS%print(choice.tags)
-%TS%%TS%# '_select_choice' is a function that will take the index of
-%TS%%TS%# your selection and continue the story.
-%TS%%TS%_select_choice(0)
-%TS%else:
-%TS%%TS%# This code runs when the story reaches it's end.
-%TS%%TS%print("The End")
+_TS_while _ink_player.can_continue:
+_TS__TS_var text = _ink_player.continue_story()
+_TS__TS_# This text is a line of text from the ink story.
+_TS__TS_# Set the text of a Label to this value to display it in your game.
+_TS__TS_print(text)
+_TS_if _ink_player.has_choices:
+_TS__TS_# 'current_choices' contains a list of the choices, as strings.
+_TS__TS_for choice in _ink_player.current_choices:
+_TS__TS__TS_print(choice.text)
+_TS__TS__TS_print(choice.tags)
+_TS__TS_# '_select_choice' is a function that will take the index of
+_TS__TS_# your selection and continue the story.
+_TS__TS__select_choice(0)
+_TS_else:
+_TS__TS_# This code runs when the story reaches it's end.
+_TS__TS_print("The End")
 
 
 func _select_choice(index):
-%TS%_ink_player.choose_choice_index(index)
-%TS%_continue_story()
+_TS__ink_player.choose_choice_index(index)
+_TS__continue_story()
 
 
 # Uncomment to bind an external function.
 #
 # func _bind_externals():
-# %TS%_ink_player.bind_external_function("<function_name>", self, "_external_function")
+# _TS__ink_player.bind_external_function("<function_name>", self, "_external_function")
 #
 #
 # func _external_function(arg1, arg2):
-# %TS%pass
+# _TS_pass
 
 
 # Uncomment to observe the variables from your ink story.
 # You can observe multiple variables by putting adding them in the array.
 # func _observe_variables():
-# %TS%_ink_player.observe_variables(["var1", "var2"], self, "_variable_changed")
+# _TS__ink_player.observe_variables(["var1", "var2"], self, "_variable_changed")
 #
 #
 # func _variable_changed(variable_name, new_value):
-# %TS%print("Variable '%s' changed to: %s" %[variable_name, new_value])
+# _TS_print("Variable '%s' changed to: %s" %[variable_name, new_value])
