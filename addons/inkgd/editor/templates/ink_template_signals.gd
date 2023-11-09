8ebb11a9bc4ecfc8e5c007ab58c5a6bc8faa5545
Avery Hiebert <averyhiebert@gmail.com>
2023-11-09 00:46:34 -0500

    Update signals template

--- addons/inkgd/editor/templates/ink_template_signals.gd
+++ addons/inkgd/editor/templates/ink_template_signals.gd
@@ -1,6 +1,6 @@
 # warning-ignore-all:return_value_discarded
 
-extends %BASE%
+extends _BASE_
 
 # ############################################################################ #
 # Imports
@@ -14,7 +14,7 @@ var InkPlayer = load("res://addons/inkgd/ink_player.gd")
 # ############################################################################ #
 
 # Alternatively, it could also be retrieved from the tree.
-# onready var _ink_player = $InkPlayer
+# @onready var _ink_player = $InkPlayer
 @onready var _ink_player = InkPlayer.new()
 
 # ############################################################################ #
@@ -22,47 +22,47 @@ var InkPlayer = load("res://addons/inkgd/ink_player.gd")
 # ############################################################################ #
 
 func _ready():
-%TS%# Adds the player to the tree.
-%TS%add_child(_ink_player)
+_TS_# Adds the player to the tree.
+_TS_add_child(_ink_player)
 
-%TS%# Replace the example path with the path to your story.
-%TS%# Remove this line if you set 'ink_file' in the inspector.
-%TS%_ink_player.ink_file = load("res://path/to/file.ink.json")
+_TS_# Replace the example path with the path to your story.
+_TS_# Remove this line if you set 'ink_file' in the inspector.
+_TS__ink_player.ink_file = load("res://path/to/file.ink.json")
 
-%TS%# It's recommended to load the story in the background. On platforms that
-%TS%# don't support threads, the value of this variable is ignored.
-%TS%_ink_player.loads_in_background = true
+_TS_# It's recommended to load the story in the background. On platforms that
+_TS_# don't support threads, the value of this variable is ignored.
+_TS__ink_player.loads_in_background = true
 
-%TS%_ink_player.connect("loaded", Callable(self, "_story_loaded"))
-%TS%_ink_player.connect("continued", Callable(self, "_continued"))
-%TS%_ink_player.connect("prompt_choices", Callable(self, "_prompt_choices"))
-%TS%_ink_player.connect("ended", Callable(self, "_ended"))
+_TS__ink_player.loaded.connect(_story_loaded)
+_TS__ink_player.continued.connect(_continued)
+_TS__ink_player.prompt_choices.connect(_prompt_choices)
+_TS__ink_player.ended.connect(_ended)
 
-%TS%# Creates the story. 'loaded' will be emitted once Ink is ready
-%TS%# continue the story.
-%TS%_ink_player.create_story()
+_TS_# Creates the story. 'loaded' will be emitted once Ink is ready
+_TS_# continue the story.
+_TS__ink_player.create_story()
 
 # ############################################################################ #
 # Signal Receivers
 # ############################################################################ #
 
 func _story_loaded(successfully: bool):
-%TS%if !successfully:
-%TS%%TS%return
+_TS_if !successfully:
+_TS__TS_return
 
-%TS%# _observe_variables()
-%TS%# _bind_externals()
+_TS_# _observe_variables()
+_TS_# _bind_externals()
 
-%TS%# Here, the story is started immediately, but it could be started
-%TS%# at a later time.
-%TS%_ink_player.continue_story()
+_TS_# Here, the story is started immediately, but it could be started
+_TS_# at a later time.
+_TS__ink_player.continue_story()
 
 
 func _continued(text, tags):
-%TS%print(text)
-%TS%# Here you could yield for an hypothetical signal, before continuing.
-%TS%# await self.event
-%TS%_ink_player.continue_story()
+_TS_print(text)
+_TS_# Here you could yield for an hypothetical signal, before continuing.
+_TS_# await self.event
+_TS__ink_player.continue_story()
 
 
 # ############################################################################ #
@@ -70,38 +70,40 @@ func _continued(text, tags):
 # ############################################################################ #
 
 func _prompt_choices(choices):
-%TS%if !choices.is_empty():
-%TS%%TS%print(choices)
+_TS_if !choices.is_empty():
+_TS__TS_for choice in choices:
+_TS__TS__TS_print(choice.text)
+_TS__TS__TS_print(choice.tags)
 
-%TS%%TS%# In a real world scenario, _select_choice' could be
-%TS%%TS%# connected to a signal, like 'Button.pressed'.
-%TS%%TS%_select_choice(0)
+_TS__TS_# In a real world scenario, _select_choice' could be
+_TS__TS_# connected to a signal, like 'Button.pressed'.
+_TS__TS__select_choice(0)
 
 
 func _ended():
-%TS%print("The End")
+_TS_print("The End")
 
 
 func _select_choice(index):
-%TS%_ink_player.choose_choice_index(index)
-%TS%_ink_player.continue_story()
+_TS__ink_player.choose_choice_index(index)
+_TS__ink_player.continue_story()
 
 
 # Uncomment to bind an external function.
 #
 # func _bind_externals():
-# %TS%_ink_player.bind_external_function("<function_name>", self, "_external_function")
+# _TS__ink_player.bind_external_function("<function_name>", self, "_external_function")
 #
 #
 # func _external_function(arg1, arg2):
-# %TS%pass
+# _TS_pass
 
 
 # Uncomment to observe the variables from your ink story.
 # You can observe multiple variables by putting adding them in the array.
 # func _observe_variables():
-# %TS%_ink_player.observe_variables(["var1", "var2"], self, "_variable_changed")
+# _TS__ink_player.observe_variables(["var1", "var2"], self, "_variable_changed")
 #
 #
 # func _variable_changed(variable_name, new_value):
-# %TS%print("Variable '%s' changed to: %s" %[variable_name, new_value])
+# _TS_print("Variable '%s' changed to: %s" %[variable_name, new_value])
